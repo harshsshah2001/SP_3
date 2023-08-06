a19ab731800e14281b657fff90d8fa3cdd67afe2
Harsh <harshsshah2001@gmail.com>
2023-08-06 22:14:51 +0530

    Commit

--- Practicals/index.js
+++ Practicals/index.js
@@ -6,6 +6,9 @@ http.createServer(function(req,res){
     else if(req.url=='/exam'){
         res.write(`<html><head><body><a href="https://drive.google.com/drive/folders/1hfcXTOyvJGNnjBw6z6ovVC-GxGJT0FY5"/>This is a exam link</body></head></html>`)
     }
+    else if(req.url=='/exam/semester_end'){
+        res.write(`<html><head><body><a href="https://result.ganpatuniversity.ac.in/">This is a Result link</body></head></html>`)
+    }
 
     res.end();
-}).listen(8087)
+}).listen(8091)
